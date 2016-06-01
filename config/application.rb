05ead3fdca0e9e2493098cab2ed021e8d9f3f87d
eLZyBee <eliot.bradshaw@gmail.com>
2016-06-01 12:31:17 -0400

    got fonts working, adapted background image to fill full screen

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module ChoreBunny
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'

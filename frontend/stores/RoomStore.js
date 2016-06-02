83c6927cd3478c0530e1a002202ce567d213cc50
eLZyBee <eliot.bradshaw@gmail.com>
2016-06-02 12:07:17 -0400

    completed RoomApiUtil and RoomStore

---
+++ frontend/stores/RoomStore.js
@@ -0,0 +1,56 @@
+var AppDispatcher = require('../dispatcher/Dispatcher'),
+  RoomConstants = require('../constants/RoomConstants'),
+  Store = require('flux/utils').Store;
+
+var RoomStore = new Store(AppDispatcher);
+
+var _rooms = {};
+var _errors;
+
+RoomStore.all = function () {
+  return _rooms;
+};
+
+RoomStore.find = function (id) {
+  return _rooms[id];
+};
+
+RoomStore.updateRooms = function (rooms) {
+  rooms.forEach(function(room) {
+    _rooms[room.id] = room;
+  });
+};
+
+RoomStore.deleteRoom = function (id) {
+  _rooms[id] = null;
+};
+
+RoomStore.setErrors = function (errors) {
+  _errors = errors;
+};
+
+RoomStore.errors = function () {
+  if (_errors) {
+    return _errors;
+  }
+};
+
+RoomStore.__onDispatch = function (payload) {
+  switch(payload.actionType) {
+    case RoomConstants.RECEIVE_ROOMS:
+      RoomStore.updateRooms(payload.rooms);
+      break;
+    case RoomConstants.RECEIVE_ROOM:
+      RoomStore.updateRooms([payload.room]);
+      break;
+    case RoomConstants.REMOVE_ROOM:
+      RoomStore.deleteRoom(payload.room.id);
+      break;
+    case RoomConstants.ERROR:
+      RoomStore.setErrors(payload.errors);
+      break;
+  }
+  RoomStore.__emitChange();
+};
+
+module.exports = RoomStore;
--- frontend/util/RoomApiUtil.js
+++ frontend/util/RoomApiUtil.js
@@ -2,6 +2,68 @@ var AppDispatcher = require('../dispatcher/Dispatcher'),
   RoomConstants = require('../constants/RoomConstants');
 
 module.exports = {
+  fetchAllRooms: function() {
+    $.ajax({
+      type: 'GET',
+      url: 'api/rooms',
+      success: this.receiveAllRooms,
+      error: this.handleError
+    });
+  },
+  fetchSingleRoom: function(id) {
+    $.ajax({
+      type: 'GET',
+      url: 'api/rooms/' + id,
+      success: this.receiveSingleRoom,
+      error: this.handleError
+    });
+  },
+  createRoom: function(room) {
+    $.ajax({
+      type: 'POST',
+      url: 'api/rooms',
+      data: {room: room},
+      success: this.receiveSingleRoom,
+      error: this.handleError
+    });
+  },
+  updateRoom: function(room) {
+    $.ajax({
+      type: 'PATCH',
+      url: 'api/rooms/' + room.id,
+      data: {room: {name: room.name}},
+      success: this.receiveSingleRoom,
+      error: this.handleError
+    });
+  },
+  destroyRoom: function(room) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'api/rooms/' + room.id,
+      success: this.removeRoom,
+      error: this.handleError
+    });
+  },
+
+  // Callback Util for above
+  receiveAllRooms: function(rooms) {
+    AppDispatcher.dispatch({
+      actionType: RoomConstants.RECEIVE_ROOMS,
+      rooms: rooms
+    });
+  },
+  receiveSingleRoom: function(room) {
+    AppDispatcher.dispatch({
+      actionType: RoomConstants.RECEIVE_ROOM,
+      room: room
+    });
+  },
+  removeRoom: function(room) {
+    AppDispatcher.dispatch({
+      actionType: RoomConstants.REMOVE_ROOM,
+      room: room
+    });
+  },
   handleError: function(error) {
     AppDispatcher.dispatch({
       actionType: RoomConstants.ERROR,

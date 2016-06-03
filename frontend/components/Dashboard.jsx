77ca40c480468b64d789b2a9bdfb089a83217e9f
eLZyBee <eliot.bradshaw@gmail.com>
2016-06-03 11:10:45 -0400

    set up dashboard styling to facilitate link to bookings

--- frontend/components/Dashboard.jsx
+++ frontend/components/Dashboard.jsx
@@ -8,6 +8,9 @@ var Dashboard = React.createClass ({
   },
   componentDidMount: function () {
   },
+  toBooking: function () {
+
+  },
   signout: function () {
     ClientActions.logout();
     this.context.router.push('/login');
@@ -22,7 +25,36 @@ var Dashboard = React.createClass ({
           </nav>
         </div>
         <div className="main-dash">
-          <h1>{'Welcome to ChoreBunny, ' + SessionStore.currentUser().name + '!'}</h1>
+          <div className="getting-started">
+            <img/>
+            <h1>{'Welcome to ChoreBunny, ' + SessionStore.currentUser().name + '!'}</h1>
+            <button className='button' onClick={this.toBooking}>Make a Booking</button>
+            <h2>How to Get Started</h2>
+            <p>Bunnies are eager for chores! Here's how it works:</p>
+            <ul>
+              <li>
+                <div className='badge'>1</div>
+                <div className='content'>
+                  <h3>Pick a Chore</h3>
+                  <p>Choose chores from a list of rooms in the house</p>
+                </div>
+              </li>
+              <li>
+                <div className='badge'>2</div>
+                <div className='content'>
+                  <h3>Get Bunny</h3>
+                  <p>We'll get you a skilled Bunny to meet your request</p>
+                </div>
+              </li>
+              <li>
+                <div className='badge'>3</div>
+                <div className='content'>
+                  <h3>Get it Done</h3>
+                  <p>Your Bunny rises to the challenge and earns BunnyPoints™</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     )

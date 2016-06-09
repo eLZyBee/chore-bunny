debedcb619c85ddadaa7560940cc4c05727dce07
eLZyBee <eliot.bradshaw@gmail.com>
2016-06-09 14:07:06 -0400

    minor styling adjustments

--- frontend/components/Dashboard.jsx
+++ frontend/components/Dashboard.jsx
@@ -1,22 +1,41 @@
 var React = require('react'),
   ClientActions = require('../actions/ClientActions'),
   SessionStore = require('../stores/SessionStore'),
+  ChoreStore = require('../stores/ChoreStore'),
   BookingIndex = require('./booking/BookingIndex'),
   RoomsIndex = require('./rooms/RoomsIndex'),
-  Footer = require('./Footer');
+  Footer = require('./Footer'),
+  Search = require('react-search');
 
 var Dashboard = React.createClass ({
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
+  componentDidMount: function () {
+    this.choreListener = ChoreStore.addListener(this._choreChange);
+    ClientActions.fetchAllChores();
+  },
+  componentWillUnmount: function () {
+    this.choreListener.remove();
+  },
+  _choreChange: function () {
+    this.chores = ChoreStore.all();
+    this.forceUpdate();
+  },
   toBooking: function () {
-    this.context.router.push('/booking');
+    // if (chore_id) {
+    //   this.context.router.push('/booking?chore=' + chore_id);
+    // } else {
+      this.context.router.push('/booking');
+    // }
   },
   signout: function () {
     ClientActions.logout();
     this.context.router.push('/login');
   },
   render: function () {
+    var items = (this.chores) ? this.chores : [];
+
     return (
       <div className="dashboard">
         <div className="header group">
@@ -28,8 +47,16 @@ var Dashboard = React.createClass ({
         <div className="main-dash">
           <div className="getting-started">
             <img src={SessionStore.currentUser().image_url}/>
-            <h1 className='welcome'>{'Welcome to ChoreBunny, ' + SessionStore.currentUser().name + '!'}</h1>
-            <button className='button' onClick={this.toBooking}>Make a Booking</button>
+            <h1 className="welcome">{"Welcome to ChoreBunny, " + SessionStore.currentUser().name + "!"}</h1>
+            <Search
+              placeholder="Search for a chore"
+              className="dash-search"
+              onClick={this.toBooking}
+              items={items}
+              keys={['name']}
+              searchKey={'name'}
+              />
+            <button className="button" onClick={this.toBooking}>Make a Booking</button>
             <BookingIndex/>
             <h2>How to Get Started</h2>
             <p>Bunnies are eager for chores! Here's how it works:</p>

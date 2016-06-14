3d8e306bd1dc96be2d0bbebb90c1445b589008ad
eLZyBee <eliot.bradshaw@gmail.com>
2016-06-14 17:58:41 -0400

    fixed bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,19 @@
-var webpack = require("webpack");
+var path = require("path");
 
 module.exports = {
   context: __dirname,
   entry: "./frontend/ChoreBunny.jsx",
   output: {
-    path: "./app/assets/javascripts",
-    filename: "bundle.js"
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
-  plugins:[
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
-      }
-    })
-  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
           presets: ['react']
@@ -31,8 +21,8 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-map',
+  devtool: 'source-maps',
   resolve: {
-    extensions: ["", ".js", ".jsx"]
+    extensions: ["", ".js", ".jsx" ]
   }
 };
